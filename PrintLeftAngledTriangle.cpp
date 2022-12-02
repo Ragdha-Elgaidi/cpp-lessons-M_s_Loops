c17b5ff161c3905fc76712a8b2fd6ec317545964
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-02 20:37:51 +0200

    Create PrintLeftAngledTriangle.cpp

---
+++ PrintLeftAngledTriangle.cpp
@@ -0,0 +1,30 @@
+/*
+Read integer N, then print a left angled triangle that has N rows
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	int row = 1;
+  
+	while (row <= N) 
+	{
+		int stars_count = 1;
+
+		while (stars_count <= row) 
+    {
+			cout << '*';
+			++stars_count;
+		}
+		cout << "\n";
+		row++;
+	}
+
+	return 0;
+}
