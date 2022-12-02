277aabe8f3c11b80f5c79ffbff4077e21f7c3024
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-02 18:05:28 +0200

    Create NumberOfDigits.cpp

---
+++ NumberOfDigits.cpp
@@ -0,0 +1,21 @@
+/*
+Read a C++ integer and count its
+number of digits
+*/
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int num;
+	cin >> num;
+
+	int digits = 0;
+
+	while (num > 0) {
+		digits += 1;
+		num = num / 10;
+	}
+	cout << digits;
+	return 0;
+}
