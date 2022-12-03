ece1644a746eb544331260a1d5022ecbf3846a51
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-03 03:38:02 +0200

    Update SpecialSum.cpp

--- SpecialSum.cpp
+++ SpecialSum.cpp
@@ -48,5 +48,27 @@ int main()
 		cout << sum << "\n";
 		T--;
 	}
+	
+	//Using for loop
+	int N, T, value;
+
+	cin >> T;
+
+	while (T--) 
+	{
+		cin >> N;
+
+		int sum = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			cin >> value;
+
+			int result = 1;
+			for (int j = 0; j < i + 1; ++j)
+				result *= value;
+			sum += result;
+		}
+		cout << sum << "\n";
+	}
 	return 0;
 }
