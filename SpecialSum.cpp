988a11b89af55f80f7a7137f92925f58ac9f9e8f
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-03 02:47:29 +0200

    Create SpecialSum.cpp

---
+++ SpecialSum.cpp
@@ -0,0 +1,52 @@
+/*
+ead integer T for number of test cases.
+● For each test case read integer N. Then read N integers a, b, c
+● Output: Compute the sum of the following expression:
+○ a, b*b, c*c*c, d*d*d*d, e*e*e*e*e……
+■ That is the k-th number is repeated k times
+*/
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int T;
+
+	cin >> T;
+
+	/**
+	 * We need 3 nested loops
+	 * loop over test cases
+	 * 	loop over reading numbers
+	 * 	  loop to repeat the number K times (multiplication)
+	 */
+
+	while (T > 0)
+	
+	{
+		int N;
+		cin >> N;
+
+		int cnt_N = 1;
+		int sum = 0;
+		while (cnt_N <= N) 
+		{
+			int value = 0;
+			cin >> value;
+
+			int cnt_deep = cnt_N;
+			int result = 1;
+
+			while (cnt_deep > 0)
+			{
+				result *= value;
+				cnt_deep--;
+			}
+			sum += result;
+			cnt_N++;
+		}
+		cout << sum << "\n";
+		T--;
+	}
+	return 0;
+}
