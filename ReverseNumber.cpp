1ab1d74b1d152b6abdfdcfb072760426c9b4f328
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-03 02:15:36 +0200

    Create ReverseNumber.cpp

---
+++ ReverseNumber.cpp
@@ -0,0 +1,26 @@
+/*
+Read an integer N, then find its reverse integer R
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	int number = 0;
+
+	while (N > 0)
+	{
+		int last_digits = N % 10;
+		N /= 10;	// remove last digit
+
+		number = number * 10 + last_digits;
+	}
+	cout << number << " " << number * 3 << "\n";
+
+	return 0;
+}
